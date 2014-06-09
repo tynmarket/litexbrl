4b7c597d2e121fcd98de8885b7357d67fc006be5
tyn-iMarket <tynmarket@gmail.com>
2014-06-10 01:05:29 +0900

    ResultForecastををリファクタリング。 中間業績予想の修正に対応。

--- lib/litexbrl/tdnet/results_forecast.rb
+++ lib/litexbrl/tdnet/results_forecast.rb
@@ -3,58 +3,100 @@ module LiteXBRL
     class ResultsForecast < FinancialInformation
       include ResultsForecastAttribute
 
-      def self.find_data(doc, xbrl, accounting_base, context)
-        # 通期予想売上高
+      SEASON_Q2 = 'AccumulatedQ2'
+      SEASON_Q4 = 'Year'
+
+      def self.read(doc)
+        xbrl_q2 = read_data doc, SEASON_Q2
+        xbrl_q4 = read_data doc, SEASON_Q4
+
+        raise StandardError.new "業績予想の修正を取得できません。" unless xbrl_q2 || xbrl_q4
+
+        data = {results_forecast: []}
+        data[:results_forecast] << xbrl_q2.attributes if xbrl_q2
+        data[:results_forecast] << xbrl_q4.attributes if xbrl_q4
+
+        data
+      end
+
+      private
+
+      def self.read_data(doc, season)
+        consolidation = find_consolidation(doc, season)
+
+        return unless consolidation
+
+        xbrl, context = find_base_data(doc, consolidation, season)
+
+        find_data(doc, xbrl, context)
+      end
+
+      #
+      # 連結・非連結を取得します
+      #
+      def self.find_consolidation(doc, season)
+        cons_current = present? find_value_tse_t_rv(doc, FORECAST_NET_SALES, "Current#{season}ConsolidatedDuration")
+        cons_prev = present? find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_SALES, "Current#{season}ConsolidatedDuration")
+        non_cons_current = present? find_value_tse_t_rv(doc, FORECAST_NET_SALES, "Current#{season}NonConsolidatedDuration")
+        non_cons_prev = present? find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_SALES, "Current#{season}NonConsolidatedDuration")
+
+        if cons_current || cons_prev
+          "Consolidated"
+        elsif non_cons_current || non_cons_prev
+          "NonConsolidated"
+        end
+      end
+
+      def self.find_base_data(doc, consolidation, season)
+        context = context_hash(consolidation, season)
+
+        xbrl = new
+
+        # 証券コード
+        xbrl.code = find_securities_code(doc, consolidation)
+        # 決算年
+        xbrl.year = find_year(doc, consolidation)
+        # 決算月
+        xbrl.month = find_month(doc, consolidation)
+        # 四半期
+        xbrl.quarter = season == SEASON_Q2 ? 2 : 4
+
+        return xbrl, context
+      end
+
+      def self.find_data(doc, xbrl, context)
+        # 通期/第2四半期予想売上高
         xbrl.forecast_net_sales = to_mill(find_value_tse_t_ed(doc, FORECAST_NET_SALES, context[:context_duration]))
-        # 通期予想営業利益
+        # 通期/第2四半期予想営業利益
         xbrl.forecast_operating_income = to_mill(find_value_tse_t_ed(doc, FORECAST_OPERATING_INCOME, context[:context_duration]))
-        # 通期予想経常利益
+        # 通期/第2四半期予想経常利益
         xbrl.forecast_ordinary_income = to_mill(find_value_tse_t_ed(doc, FORECAST_ORDINARY_INCOME, context[:context_duration]))
-        # 通期予想純利益
+        # 通期/第2四半期予想純利益
         xbrl.forecast_net_income = to_mill(find_value_tse_t_ed(doc, FORECAST_NET_INCOME, context[:context_duration]))
-        # 通期予想1株当たり純利益
+        # 通期/第2四半期予想1株当たり純利益
         xbrl.forecast_net_income_per_share = to_f(find_value_tse_t_ed(doc, FORECAST_NET_INCOME_PER_SHARE, context[:context_duration]))
 
-        # 修正前通期予想売上高
+        # 修正前通期/第2四半期予想売上高
         xbrl.previous_forecast_net_sales = to_mill(find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_SALES, context[:context_duration]))
-        # 修正前通期予想営業利益
+        # 修正前通期/第2四半期予想営業利益
         xbrl.previous_forecast_operating_income = to_mill(find_value_tse_t_rv(doc, PREVIOUS_FORECAST_OPERATING_INCOME, context[:context_duration]))
-        # 修正前通期予想経常利益
+        # 修正前通期/第2四半期予想経常利益
         xbrl.previous_forecast_ordinary_income = to_mill(find_value_tse_t_rv(doc, PREVIOUS_FORECAST_ORDINARY_INCOME, context[:context_duration]))
-        # 修正前通期予想純利益
+        # 修正前通期/第2四半期予想純利益
         xbrl.previous_forecast_net_income = to_mill(find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_INCOME, context[:context_duration]))
-        # 修正前通期予想1株当たり純利益
+        # 修正前通期/第2四半期予想1株当たり純利益
         xbrl.previous_forecast_net_income_per_share = to_f(find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_INCOME_PER_SHARE, context[:context_duration]))
 
-        # 通期予想売上高増減率
+        # 通期/第2四半期予想売上高増減率
         xbrl.change_forecast_net_sales = to_f(find_value_tse_t_rv(doc, CHANGE_NET_SALES, context[:context_duration]))
-        # 通期予想営業利益増減率
+        # 通期/第2四半期予想営業利益増減率
         xbrl.change_forecast_operating_income = to_f(find_value_tse_t_rv(doc, CHANGE_OPERATING_INCOME, context[:context_duration]))
-        # 通期予想経常利益増減率
+        # 通期/第2四半期予想経常利益増減率
         xbrl.change_forecast_ordinary_income = to_f(find_value_tse_t_rv(doc, CHANGE_ORDINARY_INCOME, context[:context_duration]))
-        # 通期予想純利益増減率
+        # 通期/第2四半期予想純利益増減率
         xbrl.change_forecast_net_income = to_f(find_value_tse_t_rv(doc, CHANGE_NET_INCOME, context[:context_duration]))
 
         xbrl
-      end
-
-      def self.find_accounting_base(doc, context, quarter)
-        return :jp
-        if find_value_tse_t_ed(doc, FORECAST_NET_INCOME[:jp], context[:context_duration])
-          :jp
-        elsif find_value_tse_t_ed(doc, FORECAST_NET_INCOME[:us], context[:context_duration])
-          :us
-        elsif find_value_tse_t_ed(doc, FORECAST_NET_INCOME[:if], context[:context_duration])
-          :if
-        elsif find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_INCOME[:jp], context[:context_duration])
-          :jp
-        elsif find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_INCOME[:us], context[:context_duration])
-          :us
-        elsif find_value_tse_t_rv(doc, PREVIOUS_FORECAST_NET_INCOME[:if], context[:context_duration])
-          :if
-        else
-          :jp
-        end
       end
 
       #
--- spec/tdnet/results_forecast_spec.rb
+++ spec/tdnet/results_forecast_spec.rb
@@ -8,93 +8,93 @@ module LiteXBRL
 
       let(:dir) { File.expand_path '../../data/tdnet/results_forecast', __FILE__ }
 
-      describe ".parse" do
+      describe ".read" do
         context '日本会計基準' do
           context "連結" do
-            let(:xbrl) { ResultsForecast.parse("#{dir}/jp-cons-2012.xbrl") }
+            let(:xbrl) { (ResultsForecast.read doc("#{dir}/jp-cons-2012.xbrl"))[:results_forecast].first }
 
             it do
-              expect(xbrl.code).to eq('4368')
-              expect(xbrl.year).to eq(2012)
-              expect(xbrl.month).to eq(3)
-              expect(xbrl.quarter).to eq(4)
+              expect(xbrl[:code]).to eq('4368')
+              expect(xbrl[:year]).to eq(2012)
+              expect(xbrl[:month]).to eq(3)
+              expect(xbrl[:quarter]).to eq(4)
 
-              expect(xbrl.forecast_net_sales).to eq(28300)
-              expect(xbrl.forecast_operating_income).to eq(3850)
-              expect(xbrl.forecast_ordinary_income).to eq(3700)
-              expect(xbrl.forecast_net_income).to eq(2400)
-              expect(xbrl.forecast_net_income_per_share).to eq(380.87)
+              expect(xbrl[:forecast_net_sales]).to eq(28300)
+              expect(xbrl[:forecast_operating_income]).to eq(3850)
+              expect(xbrl[:forecast_ordinary_income]).to eq(3700)
+              expect(xbrl[:forecast_net_income]).to eq(2400)
+              expect(xbrl[:forecast_net_income_per_share]).to eq(380.87)
 
-              expect(xbrl.previous_forecast_net_sales).to eq(30300)
-              expect(xbrl.previous_forecast_operating_income).to eq(4700)
-              expect(xbrl.previous_forecast_ordinary_income).to eq(4400)
-              expect(xbrl.previous_forecast_net_income).to eq(3050)
-              expect(xbrl.previous_forecast_net_income_per_share).to eq(484.02)
+              expect(xbrl[:previous_forecast_net_sales]).to eq(30300)
+              expect(xbrl[:previous_forecast_operating_income]).to eq(4700)
+              expect(xbrl[:previous_forecast_ordinary_income]).to eq(4400)
+              expect(xbrl[:previous_forecast_net_income]).to eq(3050)
+              expect(xbrl[:previous_forecast_net_income_per_share]).to eq(484.02)
 
-              expect(xbrl.change_forecast_net_sales).to eq(-0.066)
-              expect(xbrl.change_forecast_operating_income).to eq(-0.181)
-              expect(xbrl.change_forecast_ordinary_income).to eq(-0.159)
-              expect(xbrl.change_forecast_net_income).to eq(-0.213)
+              expect(xbrl[:change_forecast_net_sales]).to eq(-0.066)
+              expect(xbrl[:change_forecast_operating_income]).to eq(-0.181)
+              expect(xbrl[:change_forecast_ordinary_income]).to eq(-0.159)
+              expect(xbrl[:change_forecast_net_income]).to eq(-0.213)
             end
           end
         end
 
         context '米国会計基準' do
           context '連結' do
-            let(:xbrl) { ResultsForecast.parse("#{dir}/us-cons-2014.xbrl") }
+            let(:xbrl) { (ResultsForecast.read doc("#{dir}/us-cons-2014.xbrl"))[:results_forecast].first }
 
             it do
-              expect(xbrl.code).to eq('6594')
-              expect(xbrl.year).to eq(2014)
-              expect(xbrl.month).to eq(3)
-              expect(xbrl.quarter).to eq(4)
+              expect(xbrl[:code]).to eq('6594')
+              expect(xbrl[:year]).to eq(2014)
+              expect(xbrl[:month]).to eq(3)
+              expect(xbrl[:quarter]).to eq(4)
 
-              expect(xbrl.forecast_net_sales).to eq(850000)
-              expect(xbrl.forecast_operating_income).to eq(80000)
-              expect(xbrl.forecast_ordinary_income).to eq(78000)
-              expect(xbrl.forecast_net_income).to eq(55000)
-              expect(xbrl.forecast_net_income_per_share).to eq(404.26)
+              expect(xbrl[:forecast_net_sales]).to eq(850000)
+              expect(xbrl[:forecast_operating_income]).to eq(80000)
+              expect(xbrl[:forecast_ordinary_income]).to eq(78000)
+              expect(xbrl[:forecast_net_income]).to eq(55000)
+              expect(xbrl[:forecast_net_income_per_share]).to eq(404.26)
 
-              expect(xbrl.previous_forecast_net_sales).to eq(820000)
-              expect(xbrl.previous_forecast_operating_income).to eq(75000)
-              expect(xbrl.previous_forecast_ordinary_income).to eq(73000)
-              expect(xbrl.previous_forecast_net_income).to eq(53500)
-              expect(xbrl.previous_forecast_net_income_per_share).to eq(398.72)
+              expect(xbrl[:previous_forecast_net_sales]).to eq(820000)
+              expect(xbrl[:previous_forecast_operating_income]).to eq(75000)
+              expect(xbrl[:previous_forecast_ordinary_income]).to eq(73000)
+              expect(xbrl[:previous_forecast_net_income]).to eq(53500)
+              expect(xbrl[:previous_forecast_net_income_per_share]).to eq(398.72)
 
-              expect(xbrl.change_forecast_net_sales).to eq(0.037)
-              expect(xbrl.change_forecast_operating_income).to eq(0.067)
-              expect(xbrl.change_forecast_ordinary_income).to eq(0.068)
-              expect(xbrl.change_forecast_net_income).to eq(0.028)
+              expect(xbrl[:change_forecast_net_sales]).to eq(0.037)
+              expect(xbrl[:change_forecast_operating_income]).to eq(0.067)
+              expect(xbrl[:change_forecast_ordinary_income]).to eq(0.068)
+              expect(xbrl[:change_forecast_net_income]).to eq(0.028)
             end
           end
         end
 
         context 'IFRS' do
           context '連結' do
-            let(:xbrl) { ResultsForecast.parse("#{dir}/ifrs-cons-2014.xbrl") }
+            let(:xbrl) { (ResultsForecast.read doc("#{dir}/ifrs-cons-2014.xbrl"))[:results_forecast].first }
 
             it do
-              expect(xbrl.code).to eq('6779')
-              expect(xbrl.year).to eq(2014)
-              expect(xbrl.month).to eq(3)
-              expect(xbrl.quarter).to eq(4)
+              expect(xbrl[:code]).to eq('6779')
+              expect(xbrl[:year]).to eq(2014)
+              expect(xbrl[:month]).to eq(3)
+              expect(xbrl[:quarter]).to eq(4)
 
-              expect(xbrl.forecast_net_sales).to eq(51000)
-              expect(xbrl.forecast_operating_income).to eq(700)
-              expect(xbrl.forecast_ordinary_income).to eq(500)
-              expect(xbrl.forecast_net_income).to eq(400)
-              expect(xbrl.forecast_net_income_per_share).to eq(20.38)
+              expect(xbrl[:forecast_net_sales]).to eq(51000)
+              expect(xbrl[:forecast_operating_income]).to eq(700)
+              expect(xbrl[:forecast_ordinary_income]).to eq(500)
+              expect(xbrl[:forecast_net_income]).to eq(400)
+              expect(xbrl[:forecast_net_income_per_share]).to eq(20.38)
 
-              expect(xbrl.previous_forecast_net_sales).to eq(51000)
-              expect(xbrl.previous_forecast_operating_income).to eq(1500)
-              expect(xbrl.previous_forecast_ordinary_income).to eq(1100)
-              expect(xbrl.previous_forecast_net_income).to eq(1000)
-              expect(xbrl.previous_forecast_net_income_per_share).to eq(50.95)
+              expect(xbrl[:previous_forecast_net_sales]).to eq(51000)
+              expect(xbrl[:previous_forecast_operating_income]).to eq(1500)
+              expect(xbrl[:previous_forecast_ordinary_income]).to eq(1100)
+              expect(xbrl[:previous_forecast_net_income]).to eq(1000)
+              expect(xbrl[:previous_forecast_net_income_per_share]).to eq(50.95)
 
-#              expect(xbrl.change_forecast_net_sales).to eq(0.037)
-              expect(xbrl.change_forecast_operating_income).to eq(-0.533)
-              expect(xbrl.change_forecast_ordinary_income).to eq(-0.545)
-              expect(xbrl.change_forecast_net_income).to eq(-0.6)
+#              expect(xbrl[:change_forecast_net_sales]).to eq(0.037)
+              expect(xbrl[:change_forecast_operating_income]).to eq(-0.533)
+              expect(xbrl[:change_forecast_ordinary_income]).to eq(-0.545)
+              expect(xbrl[:change_forecast_net_income]).to eq(-0.6)
             end
           end
         end
