4892b7cf0e1c139d2b7935d6e9a117fa0250a281
tyn-iMarket <tynmarket@gmail.com>
2014-06-17 21:47:17 +0900

    attributesのテストの消し忘れ

--- spec/tdnet/results_forecast2_spec.rb
+++ spec/tdnet/results_forecast2_spec.rb
@@ -182,55 +182,6 @@ module LiteXBRL
         end
       end
 
-      describe "#attributes" do
-        it do
-          results_forecast = ResultsForecast2.new
-          results_forecast.code = 1111
-          results_forecast.year = 2013
-          results_forecast.month = 3
-          results_forecast.quarter = 1
-          results_forecast.forecast_net_sales = 100
-          results_forecast.forecast_operating_income = 10
-          results_forecast.forecast_ordinary_income = 11
-          results_forecast.forecast_net_income = 6
-          results_forecast.forecast_net_income_per_share = 123.1
-
-          results_forecast.previous_forecast_net_sales = 90
-          results_forecast.previous_forecast_operating_income = 9
-          results_forecast.previous_forecast_ordinary_income = 10
-          results_forecast.previous_forecast_net_income = 5
-          results_forecast.previous_forecast_net_income_per_share = 113.1
-
-          results_forecast.change_forecast_net_sales = 110
-          results_forecast.change_forecast_operating_income = 20
-          results_forecast.change_forecast_ordinary_income = 21
-          results_forecast.change_forecast_net_income = 16
-
-          attr = results_forecast.attributes
-
-          expect(attr[:code]).to eq(1111)
-          expect(attr[:year]).to eq(2013)
-          expect(attr[:month]).to eq(3)
-          expect(attr[:quarter]).to eq(1)
-          expect(attr[:forecast_net_sales]).to eq(100)
-          expect(attr[:forecast_operating_income]).to eq(10)
-          expect(attr[:forecast_ordinary_income]).to eq(11)
-          expect(attr[:forecast_net_income]).to eq(6)
-          expect(attr[:forecast_net_income_per_share]).to eq(123.1)
-
-          expect(attr[:previous_forecast_net_sales]).to eq(90)
-          expect(attr[:previous_forecast_operating_income]).to eq(9)
-          expect(attr[:previous_forecast_ordinary_income]).to eq(10)
-          expect(attr[:previous_forecast_net_income]).to eq(5)
-          expect(attr[:previous_forecast_net_income_per_share]).to eq(113.1)
-
-          expect(attr[:change_forecast_net_sales]).to eq(110)
-          expect(attr[:change_forecast_operating_income]).to eq(20)
-          expect(attr[:change_forecast_ordinary_income]).to eq(21)
-          expect(attr[:change_forecast_net_income]).to eq(16)
-        end
-      end
-
     end
   end
 end
