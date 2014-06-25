4d12efcba5f5e5bbd2fd0efdec11fd6b6d6c29ca
tyn-iMarket <tynmarket@gmail.com>
2014-06-25 15:37:34 +0900

    LiteXBRLの未実装のメソッドを削除

--- lib/litexbrl.rb
+++ lib/litexbrl.rb
@@ -5,21 +5,4 @@ require 'litexbrl/utils'
 require 'litexbrl/tdnet'
 
 module LiteXBRL
-
-  class << self
-
-    def parse(path)
-      doc = find_document(path)
-
-      xbrl = doc.parse(path)
-    end
-
-    private
-
-    def find_document(path)
-      TDnet::Summary
-    end
-
-  end
-
 end
