2e004498102343047e088dc9fc804a62ed71758b
tyn-iMarket <tshinkura@linkbal.co.jp>
2017-05-18 17:16:46 +0900

    set -xを追加

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 
 bundle exec rspec
 AWS_DEFAULT_REGION=ap-northeast-1 aws ec2 revoke-security-group-ingress --group-id sg-f8da2d9e --protocol tcp --port 80 --cidr `curl -s ifconfig.me`/32
