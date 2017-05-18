2da4de403d0812fbe0382780da02a8afac8e8d03
tyn-iMarket <tshinkura@linkbal.co.jp>
2017-05-18 17:29:48 +0900

    exitする

--- test.sh
+++ test.sh
@@ -2,4 +2,15 @@
 set -x
 
 bundle exec rspec
+RSPEC_RESULT=$?
+
 AWS_DEFAULT_REGION=ap-northeast-1 aws ec2 revoke-security-group-ingress --group-id sg-f8da2d9e --protocol tcp --port 80 --cidr `curl -s ifconfig.me`/32
+AWS_RESULT=$?
+
+if [ $RSPEC_RESULT -ne 0 ]; then
+  exit $RSPEC_RESULT
+elif [ $AWS_RESULT -ne 0 ]; then
+  exit $AWS_RESULT
+else
+  exit
+fi
