8099e8e1fa5989f7bbc7e61ae3e5c7570b0d4ed8
tyn-iMarket <tshinkura@linkbal.co.jp>
2017-05-18 17:49:31 +0900

    ifconfig.io使う

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -x
 bundle exec rspec -r rspec_junit_formatter --format RspecJunitFormatter -o $CIRCLE_TEST_REPORTS/rspec/junit.xml
 RSPEC_RESULT=$?
 
-AWS_DEFAULT_REGION=ap-northeast-1 aws ec2 revoke-security-group-ingress --group-id sg-f8da2d9e --protocol tcp --port 80 --cidr `curl -s ifconfig.me`/32
+AWS_DEFAULT_REGION=ap-northeast-1 aws ec2 revoke-security-group-ingress --group-id sg-f8da2d9e --protocol tcp --port 80 --cidr `curl -s ifconfig.io`/32
 AWS_RESULT=$?
 
 if [ $RSPEC_RESULT -ne 0 ]; then
